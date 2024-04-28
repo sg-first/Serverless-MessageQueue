e006ea9232842f84c8eb4eb075fbf62d2d272123
Mingkai Zhou <524019351@qq.com>
2024-04-28 11:49:20 +0800

    队列加锁

--- views.py
+++ views.py
@@ -1,23 +1,39 @@
 from django.http import JsonResponse, HttpResponse
 from collections import deque
+import threading
 
 queue = deque()  # 全局队列
+queue_lock = threading.Lock()
 
 def queuePop(request):
-    if queue and len(queue) > 0:
-        data = queue.popleft()
-        return JsonResponse({'popContent':data})
-    else:
-        return JsonResponse({'retMsg':'queue empty'})
+    global queue
+    global queue_lock
+    queue_lock.acquire()  # 获取锁
+
+    try:
+        if queue and len(queue) > 0:
+            data = queue.popleft()
+            return JsonResponse({'popContent':data})
+        else:
+            return JsonResponse({'retMsg':'queue empty'})
+    finally:
+        queue_lock.release()  # 释放锁
+
 
 def queueInsert(request):
-    if request.method == 'POST':
-        data = request.POST.get('insertContent')
-        if data:
-            queue.append(request.POST.get('insertContent'))
-            return JsonResponse({'insertContent':data})
+    global queue
+    global queue_lock
+    queue_lock.acquire()
+
+    try:
+        if request.method == 'POST':
+            data = request.POST.get('insertContent')
+            if data:
+                queue.append(request.POST.get('insertContent'))
+                return JsonResponse({'insertContent':data})
+            else:
+                return JsonResponse({'retMsg':'need insertContent field'})
         else:
-            return JsonResponse({'retMsg':'need insertContent field'})
-    else:
-        return JsonResponse({'retMsg':'need POST request'})
-    
+            return JsonResponse({'retMsg':'need POST request'})
+    finally:
+        queue_lock.release()
